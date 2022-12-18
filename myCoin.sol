4db465888d736ddf84b7d4274eafa8d47fb09b8b
Benedict Ojekudo <59806784+benefied@users.noreply.github.com>
2022-12-19 00:13:14 +0100

    Update myCoin.sol

--- myCoin.sol
+++ myCoin.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 //importing the erc20 standard
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-//the erc contract i named it MyCoin
+//a normal not complex erc contract i named it MyCoin
 contract MyCoin is ERC20{
 //the constructor and setup/ erc
   constructor(string memory _name, string memory _symbol, uint _amount) ERC20(_name, _symbol){
